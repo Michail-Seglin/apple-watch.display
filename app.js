7be8b3602d08084f8dda6d7965e6c52028394156
Michail-Seglin <mihailsavuskin13@gmail.com>
2023-03-02 17:13:59 +0300

    1

---
+++ app.js
@@ -0,0 +1,12 @@
+
+
+// const dinamycText = document.querySelector('.wrapper p');
+// function doDynamic(arr, dinamycText) {
+//     const index = Math.round(Math.random() * (arr.length - 1));
+//     dinamycText.innerHTML = arr[index];
+// }
+// setInterval(doDynamic, 1000, arr, dinamycText);
+
+let date = new Date();
+let a = date.getTime();
+console.log(a);
